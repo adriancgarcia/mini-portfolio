dd83145cf761ebbaa372716b50e3b0fefc44a8e9
adriancgarcia <adriancgarcia@gmail.com>
2023-07-29 13:58:39 -0500

    added project cards info. Still needs styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,24 @@
 const Projects = (props) => {
 
     //The Components Returned JSX
-    return <div className="Projects"> This is Projects</div>
+    return <div className="Projects">My Projects
+        <div className = "projectCard">
+            <h1>Project 1</h1>
+            <h3>Star Wars API</h3>
+            <a href="https:git@github.com:adriancgarcia/project1.git">Git Repo</a>
+            <a href="https://project1-starwars.netlify.app/">Deployed Site</a>
+            {/* <img src= */}
+        </div>
+
+        <div className = "projectCard">
+            <h1>Project 2</h1>
+            <h3>Photo Assistant</h3>
+            <a href="git@github.com:adriancgarcia/project2-photo-assistant.git">Git Repo</a>
+            <a href="https://project-2-app.onrender.com/">Deployed Site</a>
+            {/* <img src= */}
+        </div>
+    </div>
+
 }
 
 // export the component
